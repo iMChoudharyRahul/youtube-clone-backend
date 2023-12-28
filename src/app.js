3d75afa1d74d7a34b5575046d8c05397de0d457c
Choudhary8055 <rahulchoudhary200599@gmail.com>
2023-12-28 10:11:57 +0530

    add some comment and add require middleware

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import userRouter from "./routers/user.router.js";
 
+// Create an instance of the Express application
 const app = express();
 
-app.use("/", (req, resp)=> {
-  resp.send(<h2>Hello! Restart the Learning</h2>);
-})
+/**
+ * Page Not Found
+ */
+app.all("*", (req, res) => {
+  res.status(404).send("<h3>Page Not Found </h3>");
+});
 
-app.listen(process.env.PORT);
+// Enable Cross-Origin Resource Sharing (CORS) middleware between two different origin(fronted-backend)
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN, // reflecting the request header
+    credentials: true, // Allow credentials (e.g., cookies, HTTP authentication) to be sent with the cross-origin request
+  })
+);
 
-export { app }
+// Use the cookie-parser middleware to handle cookies in the request
+app.use(cookieParser());
+
+// Middleware for handling JSON data in the request body
+app.use(express.json({ limit: "16kb" }));
+
+// Middleware for handling URL-encoded data (e.g., form data) in the request body
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+// Middleware to serve static files and assets from the "public" directory
+app.use(express.static("public"));
+
+app.use("/api/v1/users", userRouter);
+
+export { app };
