1c7a76dd39942f76830b9a33944a78e82526d506
Choudhary8055 <rahulchoudhary200599@gmail.com>
2023-12-28 10:20:50 +0530

    handling file using multer and upload on cloudinary for get secure url

--- src/utils/cloudinary.util.js
+++ src/utils/cloudinary.util.js
@@ -1,27 +1,35 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
+// Configure Cloudinary with API credentials from environment variables
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+// Function to upload a file to Cloudinary
 const uploadOnCloudinary = async localFilePath => {
   try {
-    //local file path is not avialble
+    // Check if the local file path is not available
     if (!localFilePath) return console.log("File Path is not found");
 
+    // Upload the file to Cloudinary with auto-detected resource type
     const fileUpload = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
-    //file has been uploaded successfull
-    console.log("File is uploaded on cloudinary", response.url);
-    //remove the locally saved temporary file
+    // Log a success message with the Cloudinary URL
+    console.log("Check Full Object From Cloudinary >:", fileUpload);
+    console.log("File is uploaded on cloudinary >:", fileUpload.url);
+    // Remove the locally saved temporary file
     fs.unlinkSync(localFilePath);
+
+    // Return the Cloudinary upload response
     return fileUpload;
   } catch (error) {
+    // Remove the locally saved temporary file in case of upload failure
     fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
+    // Return null to indicate upload failure
     return null;
   }
 };
